feb3bf9a7dba5c8ed4a0fa509c88848681b99467
Crystal Finch <crystal.storm+finch@gmail.com>
2025-10-30 13:57:40 -0700

    Update fonts to Newsreader and Pirata One

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Pirata_One, Newsreader } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const pirataOne = Pirata_One({
+  weight: "400",
   subsets: ["latin"],
+  variable: "--font-pirata-one",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const newsreader = Newsreader({
   subsets: ["latin"],
+  style: ["normal", "italic"],
+  axes: ["opsz"],
+  variable: "--font-newsreader",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pirataOne.variable} ${newsreader.variable} antialiased`}
         translate="no"
       >
         {children}
