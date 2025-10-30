91940f4edf1c8b1aa8f1bf38fa152418633116b8
Crystal Finch <crystal.storm+finch@gmail.com>
2025-10-30 14:22:55 -0700

    Tweak header and navigation styles

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -4,7 +4,9 @@ import DateComponent from "@components/date/Date";
 export default function Header() {
   return (
     <header className="mb-4">
-      <h1 className="font-display pt-10 pb-2">Aprende con Noticias</h1>
+      <h1 className="font-display grow text-[3.2em] leading-[1.1] text-center pt-12 pb-4">
+        Aprende con Noticias
+      </h1>
       <DateComponent className=" text-gray-400 absolute top-4" />
       <Navigation />
     </header>
--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -4,7 +4,7 @@ export default function Navigation() {
   const categories = Object.values(CATEGORIES).map((category) => category.es);
 
   return (
-    <nav className="border-y py-3">
+    <nav className="border-gray-200 dark:border-gray-700 border-y py-3">
       <ul className="flex justify-center gap-5">
         {categories.map((category) => (
           <li key={category} className="leading-none relative top-[0.03em]">
