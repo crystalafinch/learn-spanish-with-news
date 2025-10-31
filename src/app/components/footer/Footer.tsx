1c6c667b4ef793ffe7a2e243134c4ed5501fd3fc
Crystal Finch <crystal.storm+finch@gmail.com>
2025-10-31 13:17:20 -0700

    Add footer

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="border-t border-gray-200 dark:border-gray-700 text-center py-4 text-sm text-gray-500">
+      Footer text
+    </footer>
+  );
+}
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@components/header/Header";
+import Footer from "@components/footer/Footer";
 
 export default function Home() {
   return (
     <div className="container grid grid-rows-[auto_1fr_auto] md:min-h-screen min-h-dvh max-w-6xl">
       <Header />
+      <Footer />
     </div>
   );
 }
