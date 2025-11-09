f1751d10c304292d0b5be76641bcefbb437431eb
Crystal Finch <crystal.storm+finch@gmail.com>
2025-11-08 16:56:51 -0800

    Make navigation Links

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -1,14 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import { CATEGORIES } from "@/app/consts";
 
 export default function Navigation() {
-  const categories = Object.values(CATEGORIES).map((category) => category.es);
+  const pathname = usePathname();
+  const categories = Object.entries(CATEGORIES).map(([key, value]) => ({
+    key: key === "world" ? "" : key,
+    value,
+  }));
 
   return (
     <nav className="border-y border-gray-200 py-3 dark:border-gray-700">
       <ul className="flex justify-center gap-5">
         {categories.map((category) => (
-          <li key={category} className="relative top-[0.03em] leading-none">
-            {category}
+          <li key={category.key} className="relative top-[0.03em] leading-none">
+            <Link
+              href={`/${category.key}`}
+              className={pathname === `/${category.key}` ? "underline" : ""}
+            >
+              {category.value.es}
+            </Link>
           </li>
         ))}
       </ul>
