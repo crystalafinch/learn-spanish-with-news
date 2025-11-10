a29d22b1abf1373cb3287bf7fa9f33793e93c04b
Crystal Finch <crystal.storm+finch@gmail.com>
2025-11-10 15:11:57 -0800

    Rearrange header components

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -5,11 +5,15 @@ import ThemeSelect from "@/app/components/theme-select/ThemeSelect";
 export default function Header() {
   return (
     <header className="mb-4">
-      <h1 className="font-display grow pt-12 pb-4 text-center text-[3.2em] leading-[1.1]">
-        Aprende con Noticias
-      </h1>
-      <DateComponent className="absolute top-4 text-gray-400" />
-      <ThemeSelect />
+      <div className="flex flex-col-reverse items-center gap-4 pt-4">
+        <h1 className="font-display grow pt-8 pb-4 text-center text-[3.2em] leading-[1.1]">
+          Aprende con Noticias
+        </h1>
+        <div className="flex w-full justify-between">
+          <DateComponent className="text-gray-400" />
+          <ThemeSelect />
+        </div>
+      </div>
       <Navigation />
     </header>
   );
