78e4e4e4934842a94bc3aadc93faa639a981ce4c
Crystal Finch <crystal.storm+finch@gmail.com>
2025-11-14 15:37:39 -0800

    Prettier theme select

--- src/app/components/theme-select/ThemeSelect.tsx
+++ src/app/components/theme-select/ThemeSelect.tsx
@@ -2,10 +2,40 @@
 
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { CheckIcon, MoonIcon, SunIcon, MonitorIcon } from "lucide-react";
+
+function ThemeIcon({ theme }: { theme: string }) {
+  const iconSize = "size-4";
+  switch (theme) {
+    case "system":
+      return <MonitorIcon className={iconSize} />;
+    case "dark":
+      return <MoonIcon className={iconSize} />;
+    case "light":
+      return <SunIcon className={iconSize} />;
+    default:
+      return null;
+  }
+}
 
 export default function ThemeSelect() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const themeOptions = [
+    { value: "system", label: "System" },
+    { value: "dark", label: "Dark" },
+    { value: "light", label: "Light" },
+  ];
 
   useEffect(() => {
     // This pattern is required by next-themes to prevent hydration errors
@@ -16,18 +46,32 @@ export default function ThemeSelect() {
 
   return (
     <form noValidate>
-      <label htmlFor="dark-mode-toggle">
-        Toggle Dark Mode ({theme ?? "system"})
-      </label>
-      <select
-        id="dark-mode-toggle"
-        value={theme}
-        onChange={(e) => setTheme(e.target.value)}
-      >
-        <option value="system">System</option>
-        <option value="dark">Dark 🌙</option>
-        <option value="light">Light ☀️</option>
-      </select>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon" className="rounded-full">
+            <span className="sr-only">Select Theme</span>
+            <ThemeIcon theme={theme ?? "system"} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Theme</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup
+            value={theme ?? "system"}
+            onValueChange={(value: string) => setTheme(value)}
+          >
+            {themeOptions.map((option) => (
+              <DropdownMenuRadioItem
+                key={option.value}
+                value={option.value}
+                indicatorIcon={<CheckIcon className="size-3" />}
+              >
+                {option.label}
+              </DropdownMenuRadioItem>
+            ))}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </form>
   );
 }
