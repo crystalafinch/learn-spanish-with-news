11234982fbd7177648c1e6ae6dbf5cc28537dc63
Crystal Finch <crystal.storm+finch@gmail.com>
2025-12-05 12:02:27 -0800

    Add highlight color to inline theme

--- src/app/components/news-article-parts/NewsArticleParts.tsx
+++ src/app/components/news-article-parts/NewsArticleParts.tsx
@@ -17,14 +17,10 @@ function NewsArticlePart({ part }: { part: TranslatedNewsArticlePart }) {
       <HoverCardContent
         side="top"
         align="start"
-        className="text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 w-auto origin-(--radix-hover-card-content-transform-origin) rounded-md border border-transparent bg-[var(--highlight)] p-4 px-3 py-2 text-sm font-normal shadow-md outline-hidden"
+        className="text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-highlight relative z-50 w-auto origin-(--radix-hover-card-content-transform-origin) rounded-md border border-transparent p-4 px-3 py-2 text-sm font-normal shadow-md outline-hidden"
       >
         <span className="inline-block">{part.en}</span>
-        <HoverCardArrow
-          className="fill-[var(--highlight)]"
-          width={20}
-          height={10}
-        />
+        <HoverCardArrow className="fill-highlight" width={20} height={10} />
       </HoverCardContent>
     </HoverCard>
   );
